a7311fe891cd4b90bfe4c992f4160ddddcdb1c97
114186_RameNicolas <109234443+NicolasAgustinRame@users.noreply.github.com>
2024-06-11 18:57:49 -0300

    creación del proyecto

---
+++ js/equipos.js
@@ -0,0 +1,36 @@
+var imagen = new L.Icon({
+    iconUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Escudo_del_Club_Atl%C3%A9tico_Boca_Juniors.svg/1200px-Escudo_del_Club_Atl%C3%A9tico_Boca_Juniors.svg.png',
+    iconSize: [40, 50],
+
+})
+
+var imagen2 = new L.Icon({
+    iconUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Escudo_del_C_A_River_Plate.svg/1653px-Escudo_del_C_A_River_Plate.svg.png',
+    iconSize: [40, 50],
+
+})
+
+var imagen3 = new L.Icon({
+    iconUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Escudo_Talleres_2015.svg/154px-Escudo_Talleres_2015.svg.png',
+    iconSize: [50, 50],
+})
+
+var imagen4 = new L.Icon({
+    iconUrl: 'https://upload.wikimedia.org/wikipedia/commons/8/85/Escudo_Oficial_del_Club_Atl%C3%A9tico_Belgrano.png',
+    iconSize: [50, 50],
+})
+
+var imagen5 = new L.Icon({
+    iconUrl: 'https://institutoacc.com.ar/wp-content/uploads/2017/08/escudo-Instituto-ACC-01-2.png',
+    iconSize: [50, 50],
+})
+
+var imagen6 = new L.Icon({
+    iconUrl: 'https://i.pinimg.com/originals/1a/69/26/1a692607f6ad30c9ec2570627fed2240.png',
+    iconSize: [50, 50],
+})
+
+var imagen7 = new L.Icon({
+    iconUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Escudo_del_Club_Atl%C3%A9tico_Newell%27s_Old_Boys_de_Rosario.svg/1200px-Escudo_del_Club_Atl%C3%A9tico_Newell%27s_Old_Boys_de_Rosario.svg.png',
+    iconSize: [40, 50],
+})
---
+++ js/markers.js
@@ -0,0 +1,9 @@
+var markers = [
+    L.marker([-34.635611, -58.364791], { icon: imagen }).bindPopup("Club Atlético Boca Juniors"),
+    L.marker([-34.545278, -58.449722], { icon: imagen2 }).bindPopup("Club Atlético River Plate"),
+    L.marker([-31.44896, -64.17565], { icon: imagen3 }).bindPopup("Club Atlético Talleres de Córdoba"),
+    L.marker([-31.4035, -64.206272], { icon: imagen4 }).bindPopup("Club Atlético Belgrano de Córdoba"),
+    L.marker([-31.3837, -64.1803], { icon: imagen5 }).bindPopup("Instituto Central Córdoba"),
+    L.marker([-32.913997, -60.674567], { icon: imagen6 }).bindPopup("Club Atlético Rosario Central"),
+    L.marker([-32.956056, -60.661444], { icon: imagen7 }).bindPopup("Club Atlético Newell's Old Boys")
+];
